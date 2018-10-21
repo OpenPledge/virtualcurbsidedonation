02ae2979c5e16bd4db6d0f33be417e149d8f770e
Cindy Dovinh <cindy.dovinh@gmail.com>
2018-10-20 22:10:54 -0700

    created dictionary with all items in js file

---
+++ src/page.js
@@ -0,0 +1,72 @@
+var itemList = [
+ {
+   "Image": "img/FrozenTurkey.png",
+   "Items Needed": "Whole Turkey",
+   "Serving Units": "case of (6) – 16lbs ea",
+   "Our Price": "$85",
+   "Retail Price": "$120"
+ },
+ {
+   "Image": "img/ham.png",
+   "Items Needed": "Whole Ham",
+   "Serving Units": "case of (6) – 10lbs ea",
+   "Our Price": "$60",
+   "Retail Price": "$85"
+ },
+ {
+   "Image": "img/chicken.png",
+   "Items Needed": "Chicken Breasts & Legs",
+   "Serving Units": "12lbs",
+   "Our Price": "$20",
+   "Retail Price": "$30"
+ },
+ {
+   "Image": "img/rice.png",
+   "Items Needed": "Rice",
+   "Serving Units": "40lb bag",
+   "Our Price": "$50",
+   "Retail Price": "$75"
+ },
+ {
+   "Image": "img/beans.png",
+   "Items Needed": "Beans",
+   "Serving Units": "40lb bag",
+   "Our Price": "$70",
+   "Retail Price": "$100"
+ },
+ {
+   "Image": "img/milk.png",
+   "Items Needed": "Milk",
+   "Serving Units": "case of (24) – 1gal",
+   "Our Price": "$80",
+   "Retail Price": "$118"
+ },
+ {
+   "Image": "img/onions.png",
+   "Items Needed": "Onions",
+   "Serving Units": "30lb bag",
+   "Our Price": "$30",
+   "Retail Price": "$45"
+ },
+ {
+   "Image": "img/potato.png",
+   "Items Needed": "Potatoes",
+   "Serving Units": "40lb bag",
+   "Our Price": "$65",
+   "Retail Price": "$90"
+ },
+ {
+   "Image": "img/veggies.png",
+   "Items Needed": "Canned Vegetables",
+   "Serving Units": "case of (24) – 15oz ea",
+   "Our Price": "$25",
+   "Retail Price": "$38"
+ },
+ {
+   "Image": "img/tomato.png",
+   "Items Needed": "Canned Tomatoes",
+   "Serving Units": "case of (24) – 28oz ea",
+   "Our Price": "$35",
+   "Retail Price": "$55"
+ }
+]
