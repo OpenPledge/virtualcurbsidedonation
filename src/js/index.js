9f788f80fc705695a37538966f293c86503ce86a
Shane LeBlanc <shane@xs.vc>
2018-10-21 17:25:44 -0700

    dynamic paypal donate button based on cart

--- src/js/index.js
+++ src/js/index.js
@@ -1,90 +1,9 @@
 console.log("Hello world");
 
-var itemList = [
-    {
-        "quantity": 0,
-        "image": "img/FrozenTurkey.png",
-        "itemsNeeded": "Whole Turkey",
-        "servingUnits": "case of (6) – 16lbs ea",
-        "ourPrice": "85",
-        "retailPrice": "120"
-    },
-    {
-        "quantity": 0,
-        "image": "img/ham.png",
-        "itemsNeeded": "Whole Ham",
-        "servingUnits": "case of (6) – 10lbs ea",
-        "ourPrice": "60",
-        "retailPrice": "85"
-    },
-    {
-        "quantity": 0,
-        "image": "img/chicken.png",
-        "itemsNeeded": "Chicken",
-        "servingUnits": "12lbs",
-        "ourPrice": "20",
-        "retailPrice": "30"
-    },
-    {
-        "quantity": 0,
-        "image": "img/rice.png",
-        "itemsNeeded": "rice",
-        "servingUnits": "40lb bag",
-        "ourPrice": "50",
-        "retailPrice": "75"
-    },
-    {
-        "quantity": 0,
-        "image": "img/beans.png",
-        "itemsNeeded": "Beans",
-        "servingUnits": "40lb bag",
-        "ourPrice": "70",
-        "retailPrice": "100"
-    },
-    {
-        "quantity": 0,
-        "image": "img/milk.png",
-        "itemsNeeded": "Milk",
-        "servingUnits": "case of (24) – 1gal",
-        "ourPrice": "80",
-        "retailPrice": "118"
-    },
-    {
-        "quantity": 0,
-        "image": "img/onions.png",
-        "itemsNeeded": "onions",
-        "servingUnits": "30lb bag",
-        "ourPrice": "30",
-        "retailPrice": "45"
-    },
-    {
-        "quantity": 0,
-        "image": "img/potato.png",
-        "itemsNeeded": "Potatoes",
-        "servingUnits": "40lb bag",
-        "ourPrice": "65",
-        "retailPrice": "90"
-    },
-    {
-        "quantity": 0,
-        "image": "img/veggies.png",
-        "itemsNeeded": "Canned Vegetables",
-        "servingUnits": "case of (24) – 15oz ea",
-        "ourPrice": "25",
-        "retailPrice": "38",
-    },
-    {
-        "quantity": 0,
-        "image": "img/tomato.png",
-        "itemsNeeded": "Canned Tomatoes",
-        "servingUnits": "case of (24) – 28oz ea",
-        "ourPrice": "35",
-        "retailPrice": "55"
-    }
-];
+console.log(itemList);
 
-var donationTotal = 0;
-var cart = [];
+let donationTotal = 0;
+const cart = [];
 
 function updateCartList(){
     let cartList = document.getElementById('cartItems');
@@ -92,8 +11,11 @@ function updateCartList(){
     let id = 0;
     for(let item of itemList){
         if(item.quantity > 0) {
-            cartItems += `<li> ${item.quantity} ${item.itemsNeeded}`;
-            cartItems += `<a onclick="removeFromCart(${id})" href="#" class="removebutton">Remove</a> </li>`;
+            // ideally the item quantity can be clicked to show a textbox to modify it with updateQuantity()
+            cartItems += `<li> <a id="modify-${id}">${item.quantity}</a> ${item.itemsNeeded}`;
+            cartItems += `<a onclick="removeFromCart(${id})" href="#" class="removebutton">Remove</a>`;
+            cartItems += `<br> ${item.servingUnits} </li>`;
+
         }
         id++;
     }
@@ -107,6 +29,7 @@ function removeFromCart(id){
     itemList[id].quantity = 0;
     updateCartList();
 }
+
 function updateQuantity(item, newQuantity){
     itemList[item].quantity = newQuantity;
 }
@@ -116,19 +39,68 @@ function addToCart(id){
     let itemPrice = Number(itemList[id].ourPrice);
     donationTotal += itemPrice;
     updateCartList();
-    console.log(donationTotal);
+    updateDonateButton();
 }
 
-function donateTotal(){
-    console.log("Donating total " + donationTotal);
-    let paypalDescription = ``
+
+
+function updateDonateButton(){
+    // Uses the DOM to modify the PayPal donate button with the cart contents
+
+    // Variable for collecting the whole cart details in one string (unused)
+    let paypalDescription = ``;
+
+    let donateForm = document.getElementById('donateForm');
+    let id = 1;
     itemList.forEach(item => {
         if(item.quantity > 0){
-            paypalDescription += `${item.quantity}x ${item.itemsNeeded} (${item.servingUnits}) - ${item.quantity * item.ourPrice} \n`;
+            // Setting up paypal form input elements
+            let item_name = document.createElement('input'),
+              item_number = document.createElement('input'),
+              quantity = document.createElement('input'),
+              amount = document.createElement('input');
+            item_name.type = "hidden";
+            item_name.name = "item_name_" + id;
+            item_number.type = "hidden";
+            item_number.name = "item_number_" + id;
+            quantity.type = "hidden";
+            quantity.name = "quantity_" + id;
+            amount.type = "hidden";
+            amount.name = "amount_" + id;
+            // Adding cart info
+            item_name.value = item.itemsNeeded + `(${item.servingUnits})`;
+            quantity.value = item.quantity;
+            amount.value = item.ourPrice;
+            // Add input elements into the donate button form
+            donateForm.innerHTML += item_name.outerHTML + item_number.outerHTML + quantity.outerHTML + amount.outerHTML;
+
+            //paypalDescription += `${item.quantity}x ${item.itemsNeeded} (${item.servingUnits}) - $${item.quantity * item.ourPrice} \n`;
+
+            id++;
         }
     });
+    /*
+    <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
 
-    // Send donationTotal, description to paypal
+    <!-- Identify your business so that you can collect the payments. -->
+    <input type="hidden" name="business"
+        value="donations@kcparkfriends.org">
 
+    <!-- Specify a Donate button. -->
+    <input type="hidden" name="cmd" value="_donations">
+
+    <!-- Specify details about the contribution -->
+    <input type="hidden" name="item_name" value="Friends of the Park">
+    <input type="hidden" name="item_number" value="Fall Cleanup Campaign">
+    <input type="hidden" name="currency_code" value="USD">
+
+    <!-- Display the payment button. -->
+    <input type="image" name="submit"
+    src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif"
+    alt="Donate">
+    <img alt="" width="1" height="1"
+    src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" >
+
+</form>
+     */
 }
-console.log(donationTotal)
---
+++ src/js/itemList.js
@@ -0,0 +1,82 @@
+var itemList = [
+    {
+        "quantity": 0,
+        "image": "img/FrozenTurkey.png",
+        "itemsNeeded": "Whole Turkey",
+        "servingUnits": "case of (6) – 16lbs ea",
+        "ourPrice": "85",
+        "retailPrice": "120"
+    },
+    {
+        "quantity": 0,
+        "image": "img/ham.png",
+        "itemsNeeded": "Whole Ham",
+        "servingUnits": "case of (6) – 10lbs ea",
+        "ourPrice": "60",
+        "retailPrice": "85"
+    },
+    {
+        "quantity": 0,
+        "image": "img/chicken.png",
+        "itemsNeeded": "Chicken",
+        "servingUnits": "12lbs",
+        "ourPrice": "20",
+        "retailPrice": "30"
+    },
+    {
+        "quantity": 0,
+        "image": "img/rice.png",
+        "itemsNeeded": "rice",
+        "servingUnits": "40lb bag",
+        "ourPrice": "50",
+        "retailPrice": "75"
+    },
+    {
+        "quantity": 0,
+        "image": "img/beans.png",
+        "itemsNeeded": "Beans",
+        "servingUnits": "40lb bag",
+        "ourPrice": "70",
+        "retailPrice": "100"
+    },
+    {
+        "quantity": 0,
+        "image": "img/milk.png",
+        "itemsNeeded": "Milk",
+        "servingUnits": "case of (24) – 1gal",
+        "ourPrice": "80",
+        "retailPrice": "118"
+    },
+    {
+        "quantity": 0,
+        "image": "img/onions.png",
+        "itemsNeeded": "onions",
+        "servingUnits": "30lb bag",
+        "ourPrice": "30",
+        "retailPrice": "45"
+    },
+    {
+        "quantity": 0,
+        "image": "img/potato.png",
+        "itemsNeeded": "Potatoes",
+        "servingUnits": "40lb bag",
+        "ourPrice": "65",
+        "retailPrice": "90"
+    },
+    {
+        "quantity": 0,
+        "image": "img/veggies.png",
+        "itemsNeeded": "Canned Vegetables",
+        "servingUnits": "case of (24) – 15oz ea",
+        "ourPrice": "25",
+        "retailPrice": "38",
+    },
+    {
+        "quantity": 0,
+        "image": "img/tomato.png",
+        "itemsNeeded": "Canned Tomatoes",
+        "servingUnits": "case of (24) – 28oz ea",
+        "ourPrice": "35",
+        "retailPrice": "55"
+    }
+];
