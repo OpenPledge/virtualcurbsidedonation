d3384f223bbfd2bfd7dd33b9098fca54511ff45f
Cindy Dovinh <cindy.dovinh@gmail.com>
2018-10-22 13:46:32 -0700

    adjusted donate button, moved team info to separate js file to DRY

--- src/js/index.js
+++ src/js/index.js
@@ -78,41 +78,4 @@ function updateDonateButton(){
 console.log(donationTotal)
 
 
-teaminfo = [
- {
-   "name": "Shane",
-   "image": "img/team/shane.jpeg",
-   "altname": "Shane LeBlanc",
-   "link": "https://www.shaneleblanc.net"
- },
- {
-   "name": "Maddy",
-   "image": "img/team/maddy.jpeg",
-   "altname": "Maddy Ford",
-   "link": "https://www.linkedin.com/in/maddy-ford/"
- },
- {
-   "name": "Tucker",
-   "image": "img/team/tucker.jpeg",
-   "altname": "Matthew Tucker",
-   "link": "https://www.linkedin.com/in/mtucker-18/"
- },
- {
-   "name": "Liam",
-   "image": "img/team/Liam.jpeg",
-   "altname": "Liam Patten",
-   "link": ""
- },
- {
-   "name": "Cindy",
-   "image": "img/team/cindy.jpeg",
-   "altname": "Cindy Dovinh",
-   "link": "https://www.linkedin.com/in/cdovinh/"
- },
- {
-   "name": "Kickstart Coding",
-   "image": "img/team/kickstart.png",
-   "altname": "Kickstart Coding",
-   "link": "https://www.kickstartcoding.com"
- }
-]
+
