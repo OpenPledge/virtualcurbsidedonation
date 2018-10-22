b5ba2f36ee32aa77f39157d3577cbebab82dfa00
Shane LeBlanc <shane@xs.vc>
2018-10-21 17:48:39 -0700

    remove shipping from paypal checkout

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,3 @@
-console.log("Hello world");
-
-console.log(itemList);
-
 let donationTotal = 0;
 const cart = [];
 
@@ -79,28 +75,5 @@ function updateDonateButton(){
             id++;
         }
     });
-    /*
-    <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
 
-    <!-- Identify your business so that you can collect the payments. -->
-    <input type="hidden" name="business"
-        value="donations@kcparkfriends.org">
-
-    <!-- Specify a Donate button. -->
-    <input type="hidden" name="cmd" value="_donations">
-
-    <!-- Specify details about the contribution -->
-    <input type="hidden" name="item_name" value="Friends of the Park">
-    <input type="hidden" name="item_number" value="Fall Cleanup Campaign">
-    <input type="hidden" name="currency_code" value="USD">
-
-    <!-- Display the payment button. -->
-    <input type="image" name="submit"
-    src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif"
-    alt="Donate">
-    <img alt="" width="1" height="1"
-    src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" >
-
-</form>
-     */
 }
