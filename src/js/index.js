22cf4b9c0eb564489fda090b6eb42d2d81f0319d
Shane LeBlanc <shane@xs.vc>
2018-10-24 17:04:26 -0700

    paypal button switched to donation mode, simplified updateDonateButton()

--- src/js/index.js
+++ src/js/index.js
@@ -46,41 +46,20 @@ function addToCart(id){
 
 
 function updateDonateButton(){
-    // Uses the DOM to modify the PayPal donate button with the cart contents
+    // Uses the DOM to modify the PayPal donate button with the total amount
+    // paypalDescription collects all item's names + quantities and puts it into item_name field
 
-    // Variable for collecting the whole cart details in one string (unused)
-    let paypalDescription = ``;
-
-    let donateForm = document.getElementById('donateForm');
-    let id = 1;
+    let paypalDescription = '';
     itemList.forEach(item => {
-        if(item.quantity > 0){
-            // Setting up paypal form input elements
-            let item_name = document.createElement('input'),
-              item_number = document.createElement('input'),
-              quantity = document.createElement('input'),
-              amount = document.createElement('input');
-            item_name.type = "hidden";
-            item_name.name = "item_name_" + id;
-            item_number.type = "hidden";
-            item_number.name = "item_number_" + id;
-            quantity.type = "hidden";
-            quantity.name = "quantity_" + id;
-            amount.type = "hidden";
-            amount.name = "amount_" + id;
-            // Adding cart info
-            item_name.value = `Donation of: ${item.itemsNeeded} (${item.servingUnits})`;
-            quantity.value = item.quantity;
-            amount.value = item.ourPrice;
-            // Add input elements into the donate button form
-            donateForm.innerHTML += item_name.outerHTML + item_number.outerHTML + quantity.outerHTML + amount.outerHTML;
-
-            //paypalDescription += `${item.quantity}x ${item.itemsNeeded} (${item.servingUnits}) - $${item.quantity * item.ourPrice} \n`;
-
-            id++;
+        if (item.quantity > 0) {
+            paypalDescription += `${item.quantity}x ${item.unitsReceipt} ${item.nameReceipt} `;
         }
     });
 
-}
+    let totalAmount = document.getElementById('donateTotal');
+    let combinedNames = document.getElementById('donateName')
+    totalAmount.value = donationTotal;
+    combinedNames.value = paypalDescription;
 
-console.log(donationTotal)
+
+}
