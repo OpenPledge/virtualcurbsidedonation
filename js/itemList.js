7971d33d91d51b608d39c4406b1d7e353c092369
Cindy Dovinh <cindy.dovinh@gmail.com>
2018-12-18 15:42:23 -0800

    new styling for xmas

--- js/itemList.js
+++ js/itemList.js
@@ -1,6 +1,50 @@
 var itemList = [
   {
     "quantity": 0,
+    "image": "img/socks.png",
+    "itemName": "Adult Socks",
+    "servingUnits": "30-pack",
+    "ourPrice": 25,
+    "retailPrice": 35,
+    "unitsReceipt": "pack",
+    "nameReceipt": "Socks"
+  },
+  
+  
+  {
+    "quantity": 0,
+    "image": "img/bulksocks.jpeg",
+    "itemName": "Adult Socks",
+    "servingUnits": "(bulk) 150-pack",
+    "ourPrice": 120,
+    "retailPrice": 175,
+    "unitsReceipt": "pack",
+    "nameReceipt": "Socks (bulk)"
+  },
+  
+  {
+    "quantity": 0,
+    "image": "img/kidsocks.jpg",
+    "itemName": "Kid's Socks",
+    "servingUnits": "24-pack",
+    "ourPrice": 18,
+    "retailPrice": 20,
+    "unitsReceipt": "pack",
+    "nameReceipt": "Kid's Socks"
+  },
+    {
+    "quantity": 0,
+    "image": "img/briefs.jpg",
+    "itemName": "Underwear",
+    "servingUnits": "pack of 48",
+    "ourPrice": 48,
+    "retailPrice": 60,
+    "unitsReceipt": "pack",
+    "nameReceipt": "Underwear"
+  },
+  
+  {
+    "quantity": 0,
     "image": "img/jacket.png",
     "itemName": "Winter Jacket",
     "servingUnits": "one jacket",
@@ -9,41 +53,43 @@ var itemList = [
     "unitsReceipt": "each",
     "nameReceipt": "Jacket"
   },
-  {
-    "quantity": 0,
-    "image": "img/briefs.jpg",
-    "itemName": "Underwear",
-    "servingUnits": "pack of 24",
-    "ourPrice": 21,
-    "retailPrice": 30,
-    "unitsReceipt": "pack",
-    "nameReceipt": "Underwear"
-  },
+  
     {
     "quantity": 0,
     "image": "img/raincoat.jpg",
     "itemName": "Rain Gear",
     "servingUnits": "set",
-    "ourPrice": 56,
-    "retailPrice": 30,
+    "ourPrice": 30,
+    "retailPrice": 56,
     "unitsReceipt": "set",
-    "nameReceipt": "RainGear"
+    "nameReceipt": "Rain Gear"
   },
+  {
+    "quantity": 0,
+    "image": "img/kidpjs.jpg",
+    "itemName": "Kid's Pajamas",
+    "servingUnits": "set of 5",
+    "ourPrice": 35,
+    "retailPrice": 50,
+    "unitsReceipt": "sets",
+    "nameReceipt": "Kids PJs",
+  },
+   {
+    "quantity": 0,
+    "image": "img/boots.png",
+    "itemName": "Men's Boots",
+    "servingUnits": "pair",
+    "ourPrice": 75,
+    "retailPrice": 90,
+    "unitsReceipt": "pair",
+    "nameReceipt": "Boots"
+  },
+  
   
   {
     "quantity": 0,
-    "image": "img/socks.png",
-    "itemName": "Socks",
-    "servingUnits": "pack of 60",
-    "ourPrice": 24,
-    "retailPrice": 35,
-    "unitsReceipt": "pack",
-    "nameReceipt": "Socks"
-  },
-  {
-    "quantity": 0,
-    "image": "img/ham.png",
-    "itemName": "Christmas Ham",
+    "image": "img/holidayham.png",
+    "itemName": "Holiday Ham",
     "servingUnits": "case of (6)",
 
     "ourPrice": 60,
@@ -82,47 +128,5 @@ var itemList = [
     "unitsReceipt": "bag",
     "nameReceipt": "Rice"
   },
-   {
-    "quantity": 0,
-    "image": "img/milk.png",
-    "itemName": "Milk",
-    "servingUnits": "case of (12)",
-    "ourPrice": 40,
-    "retailPrice": 99,
-    "unitsReceipt": "case",
-    "nameReceipt": "Milk"
-  },
-
-
-  {
-    "quantity": 0,
-    "image": "img/potato.png",
-    "itemName": "Potatoes",
-    "servingUnits": "20lbs bag",
-    "ourPrice": 32,
-    "retailPrice": 45,
-    "unitsReceipt": "bag",
-    "nameReceipt": "Potatoes"
-  },
-  {
-    "quantity": 0,
-    "image": "img/veggies.png",
-    "itemName": "Vegetables",
-    "servingUnits": "15lbs bag",
-    "ourPrice": 10,
-    "retailPrice": 15,
-    "unitsReceipt": "bag",
-    "nameReceipt": "Veggies"
-  },
-
-   {
-    "quantity": 0,
-    "image": "img/peanutbutter.jpeg",
-    "itemName": "Peanut&nbsp;Butter",
-    "servingUnits": "64oz jar",
-    "ourPrice": 15,
-    "retailPrice": 21,
-    "unitsReceipt": "jar",
-    "nameReceipt": "PB"
-  }
+   
 ]
