e9d9a1ebbfe8b3ea2221a8a29933ea2d677c2878
Cindy Dovinh <cindy.dovinh@gmail.com>
2019-12-16 10:50:36 -0800

    add xmas item list

---
+++ js/itemlistxmas.js
@@ -0,0 +1,128 @@
+var itemList = [
+  {
+    "quantity": 0,
+    "image": "img/christmas/jacket.png",
+    "itemName": "Winter Jacket",
+    "servingUnits": "one jacket",
+    "ourPrice": 42,
+    "retailPrice": 60,
+    "unitsReceipt": "each",
+    "nameReceipt": "Jacket"
+  },
+  {
+    "quantity": 0,
+    "image": "img/christmas/briefs.jpg",
+    "itemName": "Underwear",
+    "servingUnits": "pack of 24",
+    "ourPrice": 21,
+    "retailPrice": 30,
+    "unitsReceipt": "pack",
+    "nameReceipt": "Underwear"
+  },
+    {
+    "quantity": 0,
+    "image": "img/christmas/raincoat.jpg",
+    "itemName": "Rain Gear",
+    "servingUnits": "set",
+    "ourPrice": 56,
+    "retailPrice": 30,
+    "unitsReceipt": "set",
+    "nameReceipt": "RainGear"
+  },
+  
+  {
+    "quantity": 0,
+    "image": "img/christmas/socks.png",
+    "itemName": "Socks",
+    "servingUnits": "pack of 60",
+    "ourPrice": 24,
+    "retailPrice": 35,
+    "unitsReceipt": "pack",
+    "nameReceipt": "Socks"
+  },
+  {
+    "quantity": 0,
+    "image": "img/ham.png",
+    "itemName": "Christmas Ham",
+    "servingUnits": "case of (6)",
+
+    "ourPrice": 60,
+    "retailPrice": 85,
+    "unitsReceipt": "case",
+    "nameReceipt": "Ham"
+  },
+  	
+  {
+    "quantity": 0,
+    "image": "img/turkey.png",
+    "itemName": "Turkey",
+    "servingUnits": "case of (6)",
+    "ourPrice": 85,
+    "retailPrice": 120,
+    "unitsReceipt": "case",
+    "nameReceipt": "Turkey"
+  },
+  {
+    "quantity": 0,
+    "image": "img/chicken.png",
+    "itemName": "Chicken",
+    "servingUnits": "12lbs tray",
+    "ourPrice": 20,
+    "retailPrice": 30,
+    "unitsReceipt": "tray",
+    "nameReceipt": "Chicken"
+  },
+  {
+    "quantity": 0,
+    "image": "img/rice.png",
+    "itemName": "Rice",
+    "servingUnits": "40lbs bag",
+    "ourPrice": 50,
+    "retailPrice": 75,
+    "unitsReceipt": "bag",
+    "nameReceipt": "Rice"
+  },
+   {
+    "quantity": 0,
+    "image": "img/milk.png",
+    "itemName": "Milk",
+    "servingUnits": "case of (12)",
+    "ourPrice": 40,
+    "retailPrice": 99,
+    "unitsReceipt": "case",
+    "nameReceipt": "Milk"
+  },
+
+
+  {
+    "quantity": 0,
+    "image": "img/potato.png",
+    "itemName": "Potatoes",
+    "servingUnits": "20lbs bag",
+    "ourPrice": 32,
+    "retailPrice": 45,
+    "unitsReceipt": "bag",
+    "nameReceipt": "Potatoes"
+  },
+  {
+    "quantity": 0,
+    "image": "img/veggies.png",
+    "itemName": "Vegetables",
+    "servingUnits": "15lbs bag",
+    "ourPrice": 10,
+    "retailPrice": 15,
+    "unitsReceipt": "bag",
+    "nameReceipt": "Veggies"
+  },
+
+   {
+    "quantity": 0,
+    "image": "img/peanutbutter.jpeg",
+    "itemName": "Peanut&nbsp;Butter",
+    "servingUnits": "64oz jar",
+    "ourPrice": 15,
+    "retailPrice": 21,
+    "unitsReceipt": "jar",
+    "nameReceipt": "PB"
+  }
+]
